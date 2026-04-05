585259bcecc903c8c6b77bd13a4105b8dbe826f1
janpinol <pinoljan@gmail.com>
2026-04-05 19:07:32 +0200

    Install required fonts for polybar and rofi

--- run_once_install-extra-tools.sh
+++ run_once_install-extra-tools.sh
@@ -17,3 +17,26 @@ if [ ! -d "$HOME/.tmux" ]; then
   git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
   ln -sf "$HOME/.tmux/.tmux.conf" "$HOME/.tmux.conf"
 fi
+
+# Nerd Fonts
+mkdir -p "$HOME/.local/share/fonts"
+
+if ! fc-list | grep -qi "Iosevka Nerd Font"; then
+  cd /tmp
+  curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.zip
+  mkdir -p "$HOME/.local/share/fonts/IosevkaNerd"
+  unzip -o Iosevka.zip -d "$HOME/.local/share/fonts/IosevkaNerd"
+fi
+
+if ! fc-list | grep -qi "Hack Nerd Font"; then
+  cd /tmp
+  curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
+  mkdir -p "$HOME/.local/share/fonts/HackNerd"
+  unzip -o Hack.zip -d "$HOME/.local/share/fonts/HackNerd"
+fi
+
+if ! fc-list | grep -qi "feather"; then
+  curl -fLo "$HOME/.local/share/fonts/feather.ttf" https://github.com/AT-UI/feather-font/raw/master/dist/feather.ttf
+fi
+
+fc-cache -fv
